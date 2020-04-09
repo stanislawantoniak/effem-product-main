1ac001f2895b2673470b1d914519ba0bf70dba7e
Stanislaw Antoniak <stanislaw.h.antoniak@gmail.com>
2020-04-09 14:19:50 +0200

    user data added

--- src/index.js
+++ src/index.js
@@ -10,16 +10,27 @@ dotenv.config();
 const { buildFederatedSchema } = require('@apollo/federation');
 
 const schema = buildFederatedSchema([{
-		typeDefs,
-		resolvers
-	}]);
+	typeDefs,
+	resolvers
+}]);
 
 const server = new ApolloServer({
 	schema: schema,
 	dataSources: () => ({
 		productAPI: new ProductAPI()
-	})
-}); 
+	}),
+	context: ({ req }) => {
+		var userbase64 = req.headers['x-user-data'] || '';
+
+		const user = JSON.parse(new Buffer(userbase64, 'base64').toString());
+
+		if (!user.authenticated) throw new AuthenticationError('Unauthorized: You must pass valid user data here.');
+
+		console.log('user from upstream: ', user);
+		// add the user to the context
+		return { user };
+	}
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
 	console.log(`🚀 Server ready at ${url}`);
