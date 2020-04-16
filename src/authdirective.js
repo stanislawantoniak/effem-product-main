bc628478e4706c597ca406d979bb4af2e8eb3785
Stanislaw Antoniak <stanislaw.h.antoniak@gmail.com>
2020-04-16 15:17:44 +0200

    schema changes - not breaking

--- src/authdirective.js
+++ src/authdirective.js
@@ -2,9 +2,7 @@ const { SchemaDirectiveVisitor } = require('graphql-tools');
 const { defaultFieldResolver } = require('graphql');
 
 class AuthDirective extends SchemaDirectiveVisitor {
-	// Visitor methods for nested types like fields and arguments
-	// also receive a details object that provides information about
-	// the parent and grandparent types.
+
 	visitFieldDefinition(field) {
 
 		field._requiredAuthRole = this.args.requires;
--- src/schema.js
+++ src/schema.js
@@ -13,18 +13,28 @@ enum Role {
 }
 
 type Product @key(fields: "id") {
+  """Unique ID of the product."""
   id: ID!
   systemId: String
+  """Name of the product to be used in product feature pages."""
   name: String
+  """Brand of the product"""
   brand: String 
-  description: String 
+  """Description that is used to higlight product features to be used on product feature page. Note that new lines are presented as \n so you need to replace them with proper HTM line breaks markup before displaying."""
+  description: String
   cost: Float @auth(requires: ADMIN)
+  """Internal product name used in MARS backoffice system."""
   SAPProductTitle: String @auth(requires: ADMIN)
+  """Suggested retail price."""
   retailPrice: Float
+  """Product weight in kg."""
+  weight: Float
 }
 
 type Query {
+  """This query returns 1000 products per page. It is intended for use in product listings. Result paging to be implemented yet. We recommend to use properties very selectively."""
   products: [Product]!
+  """This query returns product by its ID. We recommend to use this query when displaying given product page."""
   product(id: ID!): Product
 }
 
